203a6469546ced41c20144ab762941ef69c5f0af
Malysheva Anastasia <anastasiamalysheva250@gmail.com>
2025-02-21 15:01:05 +0300

    last version of front

--- app/src/main/java/com/example/client/pojo/User.java
+++ app/src/main/java/com/example/client/pojo/User.java
@@ -1,7 +1,18 @@
 package com.example.client.pojo;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class User {
+    @JsonProperty
     private String name;
+
+    public User(){
+        this.name = "unknown user";
+    }
+
+    public User(String name){
+        this.name = name;
+    }
 
     public String getName(){
         return name;
