9e89588c82af986baf2b135449070850e3695e88
Anna Artamonova <anna.artamono@gmail.com>
2025-04-11 16:13:11 +0300

    add a little more style to activity_profile_settings.xml

--- app/src/main/java/com/example/musementfrontend/ProfileSettings.java
+++ app/src/main/java/com/example/musementfrontend/ProfileSettings.java
@@ -1,9 +1,13 @@
 package com.example.musementfrontend;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+import com.example.musementfrontend.util.UtilButtons;
 
 public class ProfileSettings extends AppCompatActivity {
 
@@ -13,9 +17,23 @@ public class ProfileSettings extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_profile_settings);
 
+        UtilButtons.Init(this);
+
+//        setUserAvatar();
+
         // Button saveButton = findViewById(R.id.saveButton);
         // saveButton.setOnClickListener(v -> {
         //     // Логика сохранения настроек
         // });
     }
+
+    private void setUserAvatar() {
+        // get info about user avatar
+        ImageView avatar = findViewById(R.id.avatar);
+        Glide.with(this)
+                .load("https://zefirka.club/uploads/posts/2023-01/1673278260_2-zefirka-club-p-serii-chelovek-na-avu-2.png")
+                .circleCrop()
+                .into(avatar);
+    }
+
 }
