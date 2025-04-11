32be4f20b58f6fb97b89425762b476831b83538f
Anna Artamonova <anna.artamono@gmail.com>
2025-04-11 15:52:34 +0300

    add profile a little more style to activity_profile_settings.xml

--- app/src/main/java/com/example/musementfrontend/ProfileSettings.java
+++ app/src/main/java/com/example/musementfrontend/ProfileSettings.java
@@ -1,6 +1,8 @@
 package com.example.musementfrontend;
 
 import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ProfileSettings extends AppCompatActivity {
@@ -8,6 +10,7 @@ public class ProfileSettings extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_profile_settings);
 
         // Button saveButton = findViewById(R.id.saveButton);
