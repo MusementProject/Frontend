86b8ec7300f117df3b89909398a74cd42a98c4b2
Anna Artamonova <anna.artamono@gmail.com>
2025-04-11 14:40:45 +0300

    add profile settings activity

---
+++ app/src/main/java/com/example/musementfrontend/ProfileSettings.java
@@ -0,0 +1,18 @@
+package com.example.musementfrontend;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProfileSettings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_settings);
+
+        // Button saveButton = findViewById(R.id.saveButton);
+        // saveButton.setOnClickListener(v -> {
+        //     // Логика сохранения настроек
+        // });
+    }
+}
